614ac55300acc272b57c383631c09a4e439cfa86
Carlos Alexandre <carlosalexandreoliveiraalves3@gmail.com>
2024-11-19 12:54:42 -0300

    feat:tela da dashboard

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss'
 })
-export class SideNavComponent {
-
+export class SideNavComponent{
+  isDrawerOpen = true;
 }
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatGridListModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 2, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 4, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 2, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 2, color: '#DDBDF1'},
+  ];
+}
